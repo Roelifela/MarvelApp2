a42b83d2c560d51edc31ce85a165d0fd1de71f1d
Roelifela <rodrigo_cordeiro@hotmail.com>
2019-05-27 20:18:38 -0300

    initial commit

---
+++ app/src/main/java/br/com/digitalhouse/digital/pimarvel/Home/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse.digital.pimarvel.Home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.digitalhouse.digital.pimarvel.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
